995bc8ed3b588fa954a9659599963b508f4d9f88
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-01-07 17:08:17 -0500

    update some notes and get arg parsing working nicely

--- giveMeAMove.py
+++ giveMeAMove.py
@@ -1,3 +1,18 @@
-import sys
+import sys, argparse
 
-sys.exit(4)
+def main(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b")
+    parser.add_argument("-p")
+    parser.add_argument("-t")
+    pargs = vars(parser.parse_args())
+    board, player, msTime = pargs["b"], pargs["p"], pargs["t"]
+    print("--------------------------------------------------------------")
+    print(board)
+    print(player)
+    print(msTime)
+    print("--------------------------------------------------------------")
+    sys.exit(4)
+
+if __name__ == "__main__":
+    main(sys.argv)
