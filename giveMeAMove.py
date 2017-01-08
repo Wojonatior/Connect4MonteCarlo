991eef6ac8c0ce163ebccdb44e4855db82b053c5
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-01-08 04:42:07 -0500

    implement finding horizontal winners, and the test for vertical winners

--- giveMeAMove.py
+++ giveMeAMove.py
@@ -1,5 +1,6 @@
 import sys, argparse, ast
 
+# accepts the argv object and returns the relevant 3 values as a relevant type 
 def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("-b")
@@ -9,10 +10,24 @@ def parse_args(argv):
     board, player, msTime = ast.literal_eval(pargs["b"]), 1 if pargs["p"] == "player-one" else 2, int(pargs["t"])
     return (board, player, msTime)
 
+#Prints each item in a list  with some lines ahead/after to make output easily visible
 def debug_output(valuesToPrint):
     print("----------------------------------------------------------------------------------------------------------------------------")
     print(*valuesToPrint, sep='\n')
     print("----------------------------------------------------------------------------------------------------------------------------")
+
+#retuns the player number if the board contains a winning player, 0 if no winner
+def findWin(board):
+    height = 6
+    width = 7
+    # Checking for vertical 
+    for y in range(height):
+        for x in range(width-3):
+            if board[y][x:x+4].count(1) == 4:
+                return 1
+            if board[y][x:x+4].count(2) == 4:
+                return 2
+    return 0
 
 def main(argv):
     board, player, msTime = parse_args(argv)
--- test_ai.py
+++ test_ai.py
@@ -5,3 +5,15 @@ baseInput = ['../myAI/giveMeAMove.py', '-b', '[[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[
 def test_makes_a_move():
     with pytest.raises(SystemExit):
         giveMeAMove.main(baseInput)
+
+def test_find_no_winner():
+    emptyBoard = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
+    assert giveMeAMove.findWin(emptyBoard) == 0
+
+def test_find_horizontal_winner():
+    horizontalWinBoard = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,2,2,2,0],[0,0,0,1,1,1,1]]
+    assert giveMeAMove.findWin(horizontalWinBoard) == 1
+
+def test_find_vertical_winner():
+    horizontalWinBoard = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,1],[0,0,0,0,0,2,1],[0,0,0,0,0,2,1],[0,0,0,0,0,2,1]]
+    assert giveMeAMove.findWin(horizontalWinBoard) == 1
