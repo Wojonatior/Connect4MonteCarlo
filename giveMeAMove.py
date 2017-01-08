0a606076e53a0fbb7e9e7adf02ea24b811d11001
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-01-07 19:52:31 -0500

    break out parsing input and debug output into their own functions

--- giveMeAMove.py
+++ giveMeAMove.py
@@ -1,19 +1,22 @@
 import sys, argparse, ast
 
-def main(argv):
+def parse_args(argv):
     parser = argparse.ArgumentParser()
     parser.add_argument("-b")
     parser.add_argument("-p")
     parser.add_argument("-t")
     pargs = vars(parser.parse_args())
     board, player, msTime = ast.literal_eval(pargs["b"]), 1 if pargs["p"] == "player-one" else 2, int(pargs["t"])
-    print("--------------------------------------------------------------")
-    print(board)
-    print(type(board))
-    print(type(board[1]))
-    print(player)
-    print(msTime)
-    print("--------------------------------------------------------------")
+    return (board, player, msTime)
+
+def debug_output(valuesToPrint):
+    print("----------------------------------------------------------------------------------------------------------------------------")
+    print(*valuesToPrint, sep='\n')
+    print("----------------------------------------------------------------------------------------------------------------------------")
+
+def main(argv):
+    board, player, msTime = parse_args(argv)
+    debug_output([board, type(board), player, msTime])
     sys.exit(4)
 
 if __name__ == "__main__":
