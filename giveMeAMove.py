d1e1bdd9f638ebea563b627932c224cc0ccab1d4
Jarek Wojciechowski <jarekwojo@gmail.com>
2017-01-09 06:12:08 -0500

    cleanup import statements, and most print lines, and cleanup general structure

--- giveMeAMove.py
+++ giveMeAMove.py
@@ -1,8 +1,9 @@
-import sys, argparse, ast
-from threading import Timer
+from ast import literal_eval
+from argparse import ArgumentParser
+from sys import exit, argv
 from random import choice
 from copy import deepcopy
-#TODO Cull imports to just required functions
+from time import time
 
 class MonteCarloAI(object):
     def __init__(self, board, numGames, aiPlayer, timeout):
@@ -14,38 +15,30 @@ class MonteCarloAI(object):
         self.legalMoves = findLegalMoves(self.board)
         self.winCounter = {}
         self.playCounter = {}
+        self.startTime = time()
 
     def calculate_best_move(self):
         #Bail out early here
+        if len(self.legalMoves) == 1:
+            exit(self.legalMoves[0])
         
         simulatedGames = 0
-        move_timeout = Timer(self.timeout, self.execute_best_move)
-        move_timeout.start()
+        endTime = time() + self.timeout
 
-        # for move in self.legalMoves:
-            # strBoard = str(make_move(self.board, move, self.aiPlayer))
-            # self.playCounter[strBoard] = 0
-            # self.winCounter[strBoard] = 0
-
-
-        while True:
+        while simulatedGames < self.numGames and time() < endTime:
             self.simulate_one_game(self.board)
             simulatedGames += 1
-            # if simulatedGames == self.numGames:
-                # self.execute_best_move()
 
+        self.execute_best_move()
 
     def simulate_one_game(self, board):
         visitedBoards = set()
         simulatedBoard = deepcopy(board)
-        playerToOptimize = self.aiPlayer
         noNewNodeAdded = True
-        movingPlayer = playerToOptimize
+        movingPlayer = self.aiPlayer
         winningPlayer = 0
-        numMovesMade = 0
 
-
-        # Maximum number of moves is 42, so just play util there aren't moves left
+        # Maximum number of moves is 42, so just play until there aren't moves left or someone wins
         while not winningPlayer:
             possibleMoves = findLegalMoves(simulatedBoard)
             pendingMove = choice(possibleMoves)
@@ -76,31 +69,23 @@ class MonteCarloAI(object):
         # Moves are in the form of (colToPlay, newBoardState)
         moves_to_consider = [(play, str(make_move(deepcopy(self.board), play, self.aiPlayer))) for play in self.legalMoves]
 
-        for play, boardString in moves_to_consider:
-            print("Trimmed String: ", boardString.replace(" ", ""))
-            print("wins: ", self.winCounter.get(boardString))
-            print("plays: ", self.playCounter.get(boardString))
-            print("pendingPlay: ", play)
-
         winPercentage, bestMove = max([ (self.winCounter.get(boardString, 0)/ self.playCounter.get(boardString, 1) , play) for play, boardString in moves_to_consider ])
 
-        sys.exit(bestMove)
+        print("\n")
+        print("--------------------------------------------------------------------------------")
+        print(time()- self.startTime)
+        print("--------------------------------------------------------------------------------")
+        exit(bestMove)
 
 # accepts the argv object and returns the relevant 3 values as a relevant type 
 def parse_args(argv):
-    parser = argparse.ArgumentParser()
+    parser = ArgumentParser()
     parser.add_argument("-b")
     parser.add_argument("-p")
     parser.add_argument("-t")
     pargs = vars(parser.parse_args())
-    board, player, msTime = ast.literal_eval(pargs["b"]), 1 if pargs["p"] == "player-one" else 2, int(pargs["t"])
+    board, player, msTime = literal_eval(pargs["b"]), 1 if pargs["p"] == "player-one" else 2, int(pargs["t"])
     return (board, player, msTime)
-
-#Prints each item in a list  with some lines ahead/after to make output easily visible
-def debug_output(valuesToPrint):
-    print("----------------------------------------------------------------------------------------------------------------------------")
-    print(*valuesToPrint, sep='\n')
-    print("----------------------------------------------------------------------------------------------------------------------------")
 
 #returns the player number if the board contains a winning player, 0 if no winner
 def findWin(board):
@@ -109,9 +94,11 @@ def findWin(board):
     # Checking for Horizontal
     for y in range(height):
         for x in range(width-3):
-            if board[y][x:x+4].count(1) == 4:
+            if board[y][x] == 1 and board[y][x+1] == 1 and board[y][x+2] == 1 and board[y][x+3] == 1:
+                # if board[y][x:x+4].count(1) == 4:
                 return 1
-            if board[y][x:x+4].count(2) == 4:
+            if board[y][x] == 2 and board[y][x+1] == 2 and board[y][x+2] == 2 and board[y][x+3] == 2:
+                # if board[y][x:x+4].count(2) == 4:
                 return 2
 
     # Checking for Vertical
@@ -156,8 +143,8 @@ def findLegalMoves(board):
 
 def main(argv):
     board, player, msTime = parse_args(argv)
-    ai = MonteCarloAI(board, 1000, player, msTime - 1000)
+    ai = MonteCarloAI(board, 50000, player, msTime - 1000)
     ai.calculate_best_move()
 
 if __name__ == "__main__":
-    main(sys.argv)
+    main(argv)
